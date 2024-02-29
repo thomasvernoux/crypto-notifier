b4837f143925ee4f3e71e73ab5238e271e4c7547
Thomas Vernoux <thomas@vernoux.be>
2024-02-29 12:53:23 +0100

    add usdc balance

--- fonctions_crypto.py
+++ fonctions_crypto.py
@@ -4,6 +4,8 @@ class Crypto:
         self.amount = None
         self.buy_price = None
         self.max_price = None
+        self.current_price = None
+        USDC_balance = None
 
 
 fichier = "user_data.txt"
@@ -28,6 +30,10 @@ def getCRYPTO():
             current_crypto.buy_price = float(ligne.split(":")[1].strip())
         elif ligne.startswith("maximum price"):
             current_crypto.max_price = float(ligne.split(":")[1].strip())
+        elif ligne.startswith("current price"):
+            current_crypto.current_price = float(ligne.split(":")[1].strip())
+        elif ligne.startswith("USDC balance"):
+            current_crypto.USDC_balance = float(ligne.split(":")[1].strip())
     # Ajoutez la dernière crypto après la boucle
     if current_crypto:
         cryptos.append(current_crypto)
@@ -44,5 +50,7 @@ def writeCRYPTO(cryptos):
             f.write(f"ammount : {crypto.amount}\n")
             f.write(f"buy price : {crypto.buy_price}\n")
             f.write(f"maximum price : {crypto.max_price}\n")
+            f.write(f"current price : {crypto.current_price}\n")
+            f.write(f"USDC_balance : {crypto.USDC_balance}\n")
             f.write("\n")
 
--- process_crypto.py
+++ process_crypto.py
@@ -8,6 +8,10 @@ CRYPTOS = getCRYPTO()
 
 for crypto in CRYPTOS:
     print()
-    #print(f"Nom : {crypto.name}, Amount : {crypto.amount}, Prix d'achat : {crypto.buy_price}, Prix maximum : {crypto.max_price}")
-    #current_price = cryptocompare.get_price(crypto.name, 'USD')
-    #print(current_price)
+    print(f"Nom : {crypto.name}, Amount : {crypto.amount}, Prix d'achat : {crypto.buy_price}, Prix maximum : {crypto.max_price}, Current price : {crypto.current_price}")
+    current_price = cryptocompare.get_price(crypto.name, 'USD')
+    if crypto.current_price > crypto.max_price :
+        crypto.max_price = crypto.current_price
+    crypto.USDC_balance = crypto.amount * crypto.current_price
+
+writeCRYPTO(CRYPTOS)
