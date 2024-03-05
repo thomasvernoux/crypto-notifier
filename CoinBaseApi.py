aa5574e97e49c33c19463eed4fa3b9d0b7dee531
Thomas Vernoux <thomas@vernoux.be>
2024-03-05 11:39:16 +0100

    api get price ok

--- CoinBaseApi.py
+++ CoinBaseApi.py
@@ -1,15 +1,10 @@
 
 
-from coinbase.rest import RESTClient
+from coinbase.wallet.client import Client
 import json
-import jwt
-from cryptography.hazmat.primitives import serialization
-import time
-import secrets
 
 
-
-def load_key_from_json(filename, variable):
+def load_var_from_json(filename, variable):
     """
     Load a variable from json
     @filename : path to json
@@ -20,35 +15,17 @@ def load_key_from_json(filename, variable):
     return keys[variable]
 
 
+api_key  = load_var_from_json('api_keys/api_key_001.json', "api_key")
+api_secret = load_var_from_json('api_keys/api_key_001.json', "api_secret")
 
-key_name = load_key_from_json('coinbase_cloud_api_key.json', "name")
-key_secret = load_key_from_json('coinbase_cloud_api_key.json', "privateKey")
 
-request_method = "GET"
-request_host   = "api.coinbase.com"
-request_path   = "/api/v3/brokerage/accounts"
-service_name   = "retail_rest_api_proxy"
-def build_jwt(service, uri):
-    private_key_bytes = key_secret.encode('utf-8')
-    private_key = serialization.load_pem_private_key(private_key_bytes, password=None)
-    jwt_payload = {
-        'sub': key_name,
-        'iss': "coinbase-cloud",
-        'nbf': int(time.time()),
-        'exp': int(time.time()) + 120,
-        'aud': [service],
-        'uri': uri,
-    }
-    jwt_token = jwt.encode(
-        jwt_payload,
-        private_key,
-        algorithm='ES256',
-        headers={'kid': key_name, 'nonce': secrets.token_hex()},
-    )
-    return jwt_token
-def main():
-    uri = f"{request_method} {request_host}{request_path}"
-    jwt_token = build_jwt(service_name, uri)
-    print(f"export JWT={jwt_token}")
-if __name__ == "__main__":
-    main()
+
+client = Client(api_key, api_secret)
+user = client.get_current_user()
+#print(user)
+p = client.get_buy_price(currency_pair = 'BTC-USD')
+#print(p)
+
+
+accounts = client.get_accounts()
+print(accounts)
