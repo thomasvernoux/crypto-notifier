05e98d55d5d3f441e0d146265e40471c96734ca2
Thomas Vernoux <thomas@vernoux.be>
2024-03-15 09:51:09 +0100

    add a test for calculate_sell_quantity

--- functions_basics.py
+++ functions_basics.py
@@ -105,13 +105,15 @@ def truncate_number(number_str, significant_digits=4):
 
 def test_truncate_number():
     input_values = ["1", "2.123456789876", "0.999991567", "0.00000000005679988"]
-    expected_output = ["1.000e0", "2.123e0", "0.999e-1", "5.679e-11"]
+    expected_output = ["1.000e0", "2.123e0", "9.999e-1", "5.679e-11"]
     significant_digits = 4
 
     output = [truncate_number(str(value), significant_digits) for value in input_values]
     
     if expected_output == output:
         return True
+    else : 
+        log_error_critic("error in test truncate number")
 
 def calculate_sell_quantity(product_info, sell_quantity_str):
     """
@@ -159,5 +161,81 @@ def calculate_sell_quantity(product_info, sell_quantity_str):
     
     return conform_sell_quantuty    
 
+def tests_calculate_sell_quantity():
+    """
+    Function to test the calculation of sell quantity for a given product.
+
+    Args:
+        None
+
+    Returns:
+        bool: True if the test passes, False otherwise.
+
+    Description:
+    This function tests the calculation of sell quantity for a given product.
+    It calculates the sell quantity based on the provided product information and input value.
+    Then, it compares the calculated sell quantity with the expected output.
+    If the calculated sell quantity matches the expected output, it returns True,
+    otherwise, it logs an error and returns False.
+    """
+
+    # Sample product information
+    product = {
+        'product_id': 'SEAM-USDC',
+        'price': '4.2229',
+        'price_percentage_change_24h': '-7.31530661516176',
+        'volume_24h': '49758.5',
+        'volume_percentage_change_24h': '-36.36540352788765',
+        'base_increment': '0.1',
+        'quote_increment': '0.0001',
+        'quote_min_size': '2',
+        'quote_max_size': '10000000',
+        'base_min_size': '0.1',
+        'base_max_size': '30303030.303030303030303',
+        'base_name': 'Seamless',
+        'quote_name': 'USDC',
+        'watched': False,
+        'is_disabled': False,
+        'new': False,
+        'status': 'online',
+        'cancel_only': False,
+        'limit_only': False,
+        'post_only': False,
+        'trading_disabled': False,
+        'auction_mode': False,
+        'product_type': 'SPOT',
+        'quote_currency_id': 'USDC',
+        'base_currency_id': 'SEAM',
+        'fcm_trading_session_details': None,
+        'mid_market_price': '',
+        'alias': 'SEAM-USD',
+        'alias_to': [],
+        'base_display_symbol': 'SEAM',
+        'quote_display_symbol': 'USD',
+        'view_only': False,
+        'price_increment': '0.0001',
+        'display_name': 'SEAM-USDC',
+        'product_venue': 'CBE'
+    }
+
+    # Input values for the test
+    input_values = ["0.800123798"]
+    
+    # Expected output for the test
+    expected_output = ["8.00e-1"]
+
+    # Calculating sell quantity based on the product and input values
+    output = [calculate_sell_quantity(product, value) for value in input_values]
+
+    # Comparing calculated sell quantity with expected output
+    if expected_output == output:
+        return True
+    else:
+        # Logging an error if the test fails
+        log_error_critic("tests_calculate_sell_quantity")
+
+    return False 
+
 
 test_truncate_number()
+tests_calculate_sell_quantity()
